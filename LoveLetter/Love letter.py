3f867f694118e1916a9d93c6d7df1b709ca5ab30
Currycombo <117369066+Currycombo@users.noreply.github.com>
2023-12-25 08:53:08 +0800

    Add files via upload

---
+++ LoveLetter/Love letter.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Dear Sherry,
+
+I want to express my feelings for you using the language of code.
+Last year I found a girl beautiful and sweet, thank god for bringing you back to my colorless life.
+I'm proud to tell my friends we were just kids when we fell in love, though not knowing what it was.
+Back to the day we met again, my heart still flipped for you ❤.
+
+You are the most amazing woman I have ever known, ready for every situation in life with no fear. 
+Deep in my heart, I learned the value of strength and self-reliance from you. I wanna be like you and am willing to spend my whole life feeling your brilliance.
+In the symphony of life, you are the exquisite crescendo that fills my world with boundless passion and timeless grace, painting every mundane moment with the radiant colors of love's infinite spectrum.
+I promise to be your strong support, just like how functions support parameters.
+
+You are my sunshine on a gloomy day ☀️. Without you, my life would be as empty as an unallocated memory.
+Like a nested loop, my love for you has no end.
+I cherish every moment we spend together like a precious commit in our repository of memories.
+
+I love you way more than you can imagine.
+
+Merry Christmas
+
+Yours forever,
+Ryota
+"""
+
